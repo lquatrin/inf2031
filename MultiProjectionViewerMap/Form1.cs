18b278c995b7ba19416a1f34f78b21459aa20ae8
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2016-12-02 04:04:52 -0200

    viewer map
    implementando os 3 tipos de chart

--- MultiProjectionViewerMap/Form1.cs
+++ MultiProjectionViewerMap/Form1.cs
@@ -19,17 +19,35 @@ namespace ClassCppToCS_CS
     public CppWrapper.CppInverseProjectionWrapper wrapper_inverse_projection;
     public int current_colorspace_input;
 
-    private bool clicked;
+    private string path_image;
 
     public Form1()
     {
-      clicked = false;
+      path_image = "";
       InitializeComponent();
 
       current_colorspace_input = 0;
 
       wrapper_inverse_projection = new CppWrapper.CppInverseProjectionWrapper();
       wrapper_inverse_projection.SetInputColorScapeType(0);
+
+      Chart chart = new Chart();
+
+      chart = chart1;
+
+      chart.Series.Clear();
+
+      chart.Series.Add("Loaded Points");
+      chart.Series[0].Tag = "LoadedPoints";
+      chart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+      chart.Series.Add("Input Points");
+      chart.Series[1].Tag = "InputPoints";
+      chart.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+      chart.Series.Add("Reference Points");
+      chart.Series[2].Tag = "ReferencePoints";
+      chart.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
     }
 
     private void InitializeOpenFileDialog()
@@ -62,8 +80,6 @@ namespace ClassCppToCS_CS
       label1.Text = "Start MDS";
       label1.Update();
 
-      clicked = false;
-      
       DialogResult result = OpenFileImageDialog();
       List<string> paths = new List<string>();
       if (result == DialogResult.OK) // Test result.
@@ -77,6 +93,7 @@ namespace ClassCppToCS_CS
         int counter = 0;
         foreach (String file in openFileDialog1.FileNames)
         {
+          Console.WriteLine(file);
           string files = openFileDialog1.InitialDirectory + file;
           paths.Add(files);
           counter++;
@@ -90,15 +107,15 @@ namespace ClassCppToCS_CS
 
         Chart chart = new Chart();
         chart = chart1;
-        chart.Series.RemoveAt(0);
-        chart.Series.Add("SO");
-        
+        chart.Series[0].Points.Clear();
+        chart.Series[2].Points.Clear();
+
         for (int i = 0; i < counter; i++)
         {
           string name = paths[i].Split('\\').Last();
-          chart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
           chart.Series[0].Points.AddXY(Math.Round(arrayMDS[i, 0], 5), Math.Round(arrayMDS[i, 1], 5));
           chart.Series[0].Points[i].LegendToolTip = name;
+          chart.Series[0].Points[i].Tag = paths[i];
           chart.Series[0].Points[i].ToolTip = name + "\n X= " + arrayMDS[i, 0] + " Y = " + arrayMDS[i, 1];
 
         }
@@ -198,28 +215,18 @@ namespace ClassCppToCS_CS
    
       Chart chart = chart1;
 
-      int n_reference_points;
-      if (clicked) n_reference_points = chart.Series[0].Points.Count() - 1;
-      else n_reference_points = chart.Series[0].Points.Count();
+      int n_reference_points = chart.Series[0].Points.Count();
 
       double[,] arraypoints = new double[n_reference_points, 2];
 
       List<string> paths = new List<string>();
       int counter = 0;
-      foreach (String file in openFileDialog1.FileNames)
-      {
-        string files = openFileDialog1.InitialDirectory + file;
-        paths.Add(files);
-        counter++;
-      }
-
       for (int p = 0; p < n_reference_points; p++)
       {
         DataPoint pt = chart.Series[0].Points[p];
-        if (!(pt.LegendToolTip == paths[p].Split('\\').Last()))
-        {
-          throw new System.InvalidOperationException();
-        }
+        paths.Add(pt.Tag.ToString());
+        counter++;
+
         arraypoints[p, 0] = pt.XValue;
         arraypoints[p, 1] = pt.YValues[0];
       }
@@ -227,10 +234,10 @@ namespace ClassCppToCS_CS
       double[,] input_point = new double[,] { {0,0} };
       
       Chart schart = chart1;
-      if (clicked)
+      if (schart.Series[1].Points.Count() > 0)
       {
-        input_point[0, 0] = schart.Series[0].Points[schart.Series[0].Points.Count - 1].XValue;
-        input_point[0, 1] = schart.Series[0].Points[schart.Series[0].Points.Count - 1].YValues[0];
+        input_point[0, 0] = schart.Series[1].Points[0].XValue;
+        input_point[0, 1] = schart.Series[1].Points[0].YValues[0];
       }
 
       wrapper_inverse_projection.InverseProjectionValBased(
@@ -267,24 +274,119 @@ namespace ClassCppToCS_CS
 
     private void chart1_MouseClick(object sender, MouseEventArgs e)
     {
-      var pos = e.Location;
-      Point clickPosition = pos;
-      var results = chart1.HitTest(pos.X, pos.Y, false,
-                                   ChartElementType.PlottingArea);
-      foreach (var result in results)
+      if (e.Button == MouseButtons.Left)
       {
-        if (result.ChartElementType == ChartElementType.PlottingArea)
+        var pos = e.Location;
+        Point clickPosition = pos;
+        var results = chart1.HitTest(pos.X, pos.Y, false,
+                                     ChartElementType.PlottingArea);
+        foreach (var result in results)
         {
-          if (clicked == true)
+          if (result.ChartElementType == ChartElementType.PlottingArea)
           {
-            chart1.Series[0].Points.RemoveAt(chart1.Series[0].Points.Count - 1);
+            chart1.Series[1].Points.Clear();
+            
+            var xVal = result.ChartArea.AxisX.PixelPositionToValue(pos.X);
+            var yVal = result.ChartArea.AxisY.PixelPositionToValue(pos.Y);
+
+            chart1.Series[1].Points.AddXY(Math.Round(xVal, 5), Math.Round(yVal, 5));
+            chart1.Series[1].Points[0].LegendToolTip = "userpoint";
+            chart1.Series[1].Points[0].ToolTip = "userpoint" + "\n X= " + xVal + " Y = " + yVal;
           }
-          var xVal = result.ChartArea.AxisX.PixelPositionToValue(pos.X);
-          var yVal = result.ChartArea.AxisY.PixelPositionToValue(pos.Y);
-          clicked = true;
-          chart1.Series[0].Points.AddXY(Math.Round(xVal, 5), Math.Round(yVal, 5));
-          chart1.Series[0].Points[chart1.Series[0].Points.Count - 1].LegendToolTip = "userpoint";
-          chart1.Series[0].Points[chart1.Series[0].Points.Count - 1].ToolTip = "userpoint" + "\n X= " + xVal + " Y = " + yVal;
+        }
+      }
+      if (e.Button == MouseButtons.Right)
+      {
+        Chart chart = chart1;
+        Series scol = chart.Series[0];
+        if (scol.Points.Count > 0)
+        {
+          var pos = e.Location;
+          Point clickPosition = pos;
+          var results = chart1.HitTest(pos.X, pos.Y, false, ChartElementType.PlottingArea);
+ 
+          foreach (var result in results)
+          {
+            if (result.ChartElementType == ChartElementType.PlottingArea)
+            {
+              var xpos = result.ChartArea.AxisX.PixelPositionToValue(pos.X);
+              var ypos = result.ChartArea.AxisY.PixelPositionToValue(pos.Y);
+
+              int m_id = 0;
+              double m_dist = Math.Sqrt(
+                Math.Pow(scol.Points[0].XValue - xpos, 2.0)
+                +
+                Math.Pow(scol.Points[0].YValues[0] - ypos, 2.0)
+              );
+              
+              int n_reference_points = scol.Points.Count();
+              for (int p = 1; p < n_reference_points; p++)
+              {
+                DataPoint pt = scol.Points[p];
+
+                double dist = Math.Sqrt(
+                  Math.Pow(scol.Points[p].XValue - xpos, 2.0)
+                  +
+                  Math.Pow(scol.Points[p].YValues[0] - ypos, 2.0)
+                );
+                if (dist < m_dist)
+                {
+                  m_dist = dist;
+                  m_id = p;
+                }
+              }
+              
+              chart.Series[2].Points.Add(chart.Series[0].Points[m_id]);
+              chart.Series[0].Points.RemoveAt(m_id);
+            }
+          }
+        }
+      }
+      if (e.Button == MouseButtons.Middle)
+      {
+        Chart chart = chart1;
+        Series scol = chart.Series[2];
+        if (scol.Points.Count > 0)
+        {
+          var pos = e.Location;
+          Point clickPosition = pos;
+          var results = chart1.HitTest(pos.X, pos.Y, false, ChartElementType.PlottingArea);
+
+          foreach (var result in results)
+          {
+            if (result.ChartElementType == ChartElementType.PlottingArea)
+            {
+              var xpos = result.ChartArea.AxisX.PixelPositionToValue(pos.X);
+              var ypos = result.ChartArea.AxisY.PixelPositionToValue(pos.Y);
+
+              int m_id = 0;
+              double m_dist = Math.Sqrt(
+                Math.Pow(scol.Points[0].XValue - xpos, 2.0)
+                +
+                Math.Pow(scol.Points[0].YValues[0] - ypos, 2.0)
+              );
+
+              int n_reference_points = scol.Points.Count();
+              for (int p = 1; p < n_reference_points; p++)
+              {
+                DataPoint pt = scol.Points[p];
+
+                double dist = Math.Sqrt(
+                  Math.Pow(scol.Points[p].XValue - xpos, 2.0)
+                  +
+                  Math.Pow(scol.Points[p].YValues[0] - ypos, 2.0)
+                );
+                if (dist < m_dist)
+                {
+                  m_dist = dist;
+                  m_id = p;
+                }
+              }
+
+              chart.Series[0].Points.Add(chart.Series[2].Points[m_id]);
+              chart.Series[2].Points.RemoveAt(m_id);
+            }
+          }
         }
       }
     }
